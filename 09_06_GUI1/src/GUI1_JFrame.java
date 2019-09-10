770408010f2a819b88e856b3a0373d3ee7d05fcf
ChaeLin <chaenini71@gmail.com>
2019-09-11 08:46:29 +0900

    frame, panel, label,button

--- 09_06_GUI1/src/GUI1_JFrame.java
+++ 09_06_GUI1/src/GUI1_JFrame.java
@@ -2,10 +2,20 @@ import javax.swing.*;
 
 public class GUI1_JFrame {
     public static void main(String[] args) {
+        //창
         JFrame frame = new JFrame("참이름");
+        //여러개 넣을 수 있는 주머니 ?
+        JPanel panel = new JPanel();
+        //글씨
         JLabel lable = new JLabel("아싸 어쩌다 금요일");
+        //버튼
+        JButton button = new JButton("오예!");
 
-        frame.add(lable);
+        //창에 끼워넣기
+        panel.add(lable);
+        panel.add(button);
+        frame.add(panel);
+
 
         frame.pack();
         frame.setVisible(true);
